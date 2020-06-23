3d88f797652f0b7f3f35de562269048b8bf8af64
Creeper Lv <creeperlv38@outlook.com>
2020-06-23 16:47:15 +0800

    Start to make installer v3.

---
+++ Assets/InstallerBaseCode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstallerBaseCode : MonoBehaviour
+{
+    public Canvas baseCanvas;
+    void Start()
+    {
+        try
+        {
+
+            baseCanvas.GetComponent<CanvasScaler>().scaleFactor = Screen.dpi / 100f;
+        }
+        catch (System.Exception)
+        {
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
